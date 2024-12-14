94b5ca0643f88dac34e512dd238d9d2f835398fb
Manelms <manelms2000@gmail.com>
2024-12-15 00:50:59 +0100

    wokwi simulator files added

--- main/SPI_Comunication.h
+++ main/SPI_Comunication.h
@@ -4,16 +4,16 @@
 #include <stdint.h>
 #include <esp_err.h>
 #include "driver/spi_master.h"
-#include "esp_log.h" // Include for logging macros
+#include "esp_log.h" 
 #include "driver/gpio.h"
 
-// Define constants or macros
+// Define SPI pins
 #define PIN_NUM_MISO GPIO_NUM_19
 #define PIN_NUM_SCLK GPIO_NUM_18
 #define PIN_NUM_CS   GPIO_NUM_5
 
 // Declare functions
-esp_err_t init_spi_bus(void);
+esp_err_t init_spi_bus(void); 
 esp_err_t add_max6675_device(spi_device_handle_t *handle);
 float read_max6675(spi_device_handle_t handle);
 
--- main/main.c
+++ main/main.c
@@ -7,26 +7,31 @@
 #include "driver/gpio.h"
 
 void app_main(void) {
+    
+    
+    //initialize spi
     esp_err_t ret = init_spi_bus();
-    if (ret != ESP_OK) {
-        ESP_LOGE("MAIN", "Failed to initialize SPI bus");
-        return;
-    }
-
+    
+    /* adding spi device 
+    This initializes the internal structures for a device,
+     plus allocates a CS pin on the indicated SPI master peripheral 
+     and routes it to the indicated GPIO. 
+     All SPI master devices have three CS pins and can 
+     thus control up to three devices.
+     */ 
     spi_device_handle_t max6675;
-    ret = add_max6675_device(&max6675);
-    if (ret != ESP_OK) {
-        ESP_LOGE("MAIN", "Failed to add MAX6675 device");
-        return;
-    }
+    ret = add_max6675_device(&max6675);           
+  
 
     while (1) {
+        //read the temperature from the amplifier of the sensro (MAX6675)
         float temperature = read_max6675(max6675);
         if (temperature >= 0) {
             ESP_LOGI("MAIN", "Temperature: %.2f°C", temperature);
         } else {
             ESP_LOGE("MAIN", "Failed to read temperature");
         }
+        
         vTaskDelay(pdMS_TO_TICKS(1000)); // Delay 1 second
     }
 }
